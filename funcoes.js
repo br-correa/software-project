a77a665f19690014416664d103ad8d5df933819a
Bruno Gonzaga Teixeira Correa <bteixeiracorrea@gmail.com>
2023-10-15 16:38:02 -0300

    Teste-v1
    Teste v1

--- funcoes.js
+++ funcoes.js
@@ -6,12 +6,33 @@ function redirecionarParaPaginaDeLogin() {
 // Adicione um ouvinte de evento ao botão "Voltar"
 document.getElementById("btn-voltar").addEventListener("click", redirecionarParaPaginaDeLogin);
 
+function validateAndRedirect() {
+    // Obtenha os valores do campo de usuário e senha
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
 
-function entrarLogin() {
-    // Redireciona o usuário para a página de login
-    window.location.href = "./home.html";
+    // Faça uma requisição AJAX para verificar a autenticação no servidor (assumindo que você está usando um servidor para autenticação)
+    // Aqui está um exemplo simples, você deve ajustá-lo para se adequar ao seu sistema de autenticação.
+    $.ajax({
+        type: "POST",
+        url: "sua_api_de_autenticacao.php", // Substitua pela URL da sua API de autenticação
+        data: {
+            username: username,
+            password: password
+        },
+        success: function (response) {
+            // Se a autenticação for bem-sucedida, redirecione para a página home.html
+            if (response === "autenticado") {
+                window.location.href = "home.html";
+            } else {
+                // Caso contrário, exiba uma mensagem de erro
+                alert("Nome de usuário ou senha incorretos. Tente novamente.");
+            }
+        },
+        error: function (error) {
+            // Lidar com erros de requisição, se necessário
+            console.error("Erro na requisição: " + error);
+        }
+    });
 }
-
-// Adicione um ouvinte de evento ao botão "Entrar"
-document.getElementById("btn-enviar").addEventListener("click", entrarLogin);
 
