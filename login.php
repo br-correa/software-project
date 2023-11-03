d5ff88723d538ba653b32386007af32757dc4033
Bruno Gonzaga Teixeira Correa <bteixeiracorrea@gmail.com>
2023-11-03 12:27:29 -0300

    login

--- login.php
+++ login.php
@@ -1,44 +1,11 @@
 <?php
-// Conectar ao banco de dados (substitua as credenciais do banco de dados)
-$servername = "seuservidordobanco";
-$username = "seunome";
-$password = "suasenha";
-$dbname = "NomeDoBancoDeDados";
+session_start();
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+// Destruir a sessão
+session_destroy();
 
-// Verificar a conexão
-if ($conn->connect_error) {
-    die("Erro na conexão com o banco de dados: " . $conn->connect_error);
-}
-
-// Receber os valores do formulário de login
-$email = $_POST['email'];
-$senha = $_POST['senha']; // Lembre-se de armazenar senhas de forma segura (hash e salting)
-
-// Consultar a tabela cadastro-de-usuario
-$sql = "SELECT * FROM cadastro_de_usuario WHERE email = '$email'";
-
-$result = $conn->query($sql);
-
-if ($result->num_rows == 1) {
-    // Usuário encontrado, verifique a senha
-    $row = $result->fetch_assoc();
-    $hashed_password = $row['senha']; // Recupere a senha hash armazenada no banco
-
-    if (password_verify($senha, $hashed_password)) {
-        // Senha correta, login bem-sucedido
-        session_start();
-        $_SESSION['usuario_id'] = $row['id']; // Você pode armazenar outros dados da sessão, se necessário
-        header("Location: painel-de-controle.php"); // Redirecionar para o painel de controle
-    } else {
-        // Senha incorreta
-        echo "Senha incorreta. Tente novamente.";
-    }
-} else {
-    // Usuário não encontrado
-    echo "Usuário não encontrado. Verifique o e-mail e a senha.";
-}
-
-$conn->close();
+// Redirecionar para a página de login ou outra página desejada
+header("Location: home.html"); // Substitua "login.php" pelo nome da sua página de login
+exit();
 ?>
+
