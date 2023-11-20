e3f48a245dcccfd1f206f1a5d7c45231837b0a5d
Bruno Gonzaga Teixeira Correa <bteixeiracorrea@gmail.com>
2023-11-20 00:57:33 -0300

    Update home.php

--- home.php
+++ home.php
@@ -9,3 +9,21 @@ $email = isset($_SESSION['email']) ? $_SESSION['email'] : '';
 session_write_close();
 
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <!-- Seu cabeçalho aqui -->
+</head>
+<body>
+    <!-- Adicione esta linha onde você deseja exibir o e-mail -->
+    <span id="user-email"><?php echo $email; ?></span>
+    
+    <!-- Outro conteúdo da página -->
+
+    <script>
+        // Redirecionamento para home.html sem esperar
+        window.location.href = "home.html";
+    </script>
+</body>
+</html>
