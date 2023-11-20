ddef0fc6937e4eaf46f778ff88a30264df20eea8
Bruno Gonzaga Teixeira Correa <bteixeiracorrea@gmail.com>
2023-11-19 21:37:26 -0300

    Update login.php
    Teste

--- login.php
+++ login.php
@@ -1,42 +1,44 @@
 <?php
-session_start(); // Inicie a sessão para lidar com as variáveis de sessão
+session_start();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Verifica se o formulário foi enviado
-
-    // Conexão com o banco de dados
     include("criar-conexao-db.php");
 
     // Obtém os valores do formulário
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    // Consulta SQL para verificar as credenciais do usuário
-    $sql = "SELECT * FROM tb_cadastro_de_usuarios WHERE email = ? AND senha = ?";
-
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ss", $email, $password);
-    $stmt->execute();
-    $result = $stmt->get_result();
-
-    if ($result->num_rows === 1) {
-        // As credenciais estão corretas, o usuário está logado
-        $_SESSION['email'] = $email;
-
-        // Inserir um registro de login na tabela tb_login
-        $data_hora_login = date('Y-m-d H:i:s'); // Obtém a data e hora atuais
-        $stmt = $conn->prepare("INSERT INTO tb_login (email, data_hora_login) VALUES (?, ?)");
-        $stmt->bind_param("ss", $email, $data_hora_login);
-        $stmt->execute();
-        
-        $stmt->close();
-
-        $conn->close();
-        header('Location: home.html'); // Redireciona para a página de painel do usuário
-        exit(); // Certifique-se de sair após redirecionar
+    // Validação básica
+    if (empty($email) || empty($password)) {
+        $loginError = "Preencha todos os campos.";
     } else {
-        // Credenciais incorretas
-        $loginError = "Credenciais incorretas. Tente novamente.";
+        // Consulta SQL usando instrução preparada
+        $sql = "SELECT * FROM tb_cadastro_de_usuarios WHERE email = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows === 1) {
+            $row = $result->fetch_assoc();
+            if (password_verify($password, $row['senha'])) {
+                $_SESSION['email'] = $email;
+
+                $data_hora_login = date('Y-m-d H:i:s');
+                $stmt = $conn->prepare("INSERT INTO tb_login (email, data_hora_login) VALUES (?, ?)");
+                $stmt->bind_param("ss", $email, $data_hora_login);
+                $stmt->execute();
+
+                $stmt->close();
+                $conn->close();
+                header('Location: home.html');
+                exit();
+            } else {
+                $loginError = "Credenciais incorretas. Tente novamente.";
+            }
+        } else {
+            $loginError = "Credenciais incorretas. Tente novamente.";
+        }
     }
 
     $stmt->close();
